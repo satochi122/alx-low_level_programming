1017b0bf6d20a212d2019928f4f824126a138f54
satochi122 <134384130+satochi122@users.noreply.github.com>
2023-10-26 21:25:57 +0200

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+	return (0);
+}
