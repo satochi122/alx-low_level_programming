97133fd955457624962260563f10072197957e20
satochi122 <134384130+satochi122@users.noreply.github.com>
2023-10-26 21:21:07 +0200

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("but the outcome is a piece of art,\n");
+	return (0);
+}
